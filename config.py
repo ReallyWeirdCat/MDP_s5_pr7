c538f9aeae3d86e0700f40ad714bee0ac2f1e293
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-31 13:31:50 +0300

    Config

---
+++ config.py
@@ -0,0 +1,18 @@
+from keyboard import Key
+
+MAZE_SIZE = 16
+SCR_WIDTH = 800
+SCR_HEIGHT = 600
+FPS = 60
+
+COL_PRIMARY = (0, 0, 0)
+COL_SECONDARY = (128, 128, 128)
+COL_HIGHLIGHT = (0, 255, 0)
+COL_DANGER = (255, 0, 0)
+COL_BACKGROUND = (255, 255, 255)
+
+# Keybinds
+KEY_UP = Key.up
+KEY_DOWN = Key.down
+KEY_LEFT = Key.left
+KEY_RIGHT = Key.right
