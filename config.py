46d0f3d5e10ab4db914d39a259f6f19412a32836
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-31 13:35:23 +0300

    Removed keybinds from config

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-from keyboard import Key
-
 MAZE_SIZE = 16
 SCR_WIDTH = 800
 SCR_HEIGHT = 600
@@ -10,9 +8,3 @@ COL_SECONDARY = (128, 128, 128)
 COL_HIGHLIGHT = (0, 255, 0)
 COL_DANGER = (255, 0, 0)
 COL_BACKGROUND = (255, 255, 255)
-
-# Keybinds
-KEY_UP = Key.up
-KEY_DOWN = Key.down
-KEY_LEFT = Key.left
-KEY_RIGHT = Key.right
